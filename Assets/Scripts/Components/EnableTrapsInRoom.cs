98667c9b4502d9ec3b22f792cc78d46bc82944ee
Brad <bbenton91@gmail.com>
2020-08-11 01:06:43 -0400

    Added a script to enable/disable a room of traps based on a trigger

---
+++ Assets/Scripts/Components/EnableTrapsInRoom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableTrapsInRoom : MonoBehaviour
+{
+
+    public TriggerArea Area;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        BroadcastMessage("SetTrapActive", false, SendMessageOptions.DontRequireReceiver);
+
+        Area.AddOntriggerEnterEvent(ActivateTraps);
+        Area.AddOntriggerExitEvent(DeactivateTraps);
+    }
+
+    void DeactivateTraps(Collider2D collider, TriggerArea area)
+        => BroadcastMessage("SetTrapActive", false);
+
+    void ActivateTraps(Collider2D collider, TriggerArea area)
+     => BroadcastMessage("SetTrapActive", true);
+}
